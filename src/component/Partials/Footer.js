6153163f6478fd46b70ec06d4ea695c5a485142e
GFT <gaaftechsolution@gmail.com>
2024-04-20 14:52:06 +0100

    initial commit

--- src/component/Partials/Footer.js
+++ src/component/Partials/Footer.js
@@ -3,6 +3,69 @@ import { FaFacebook, FaInstagram, FaLinkedin, FaGoogle, FaPinterest, FaTwitter }
 
 const Footer = () => {
     return (
+        <footer className="bg-dark text-white">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3">
+                        <h2>About</h2>
+                        <p>Careers</p>
+                        <p>Partnership</p>
+                        <p>Our Team</p>
+                        <p>Privacy Policy</p>
+                    </div>
+                    <div className="col-md-3">
+                        <h2>Our Service</h2>
+                        <p>Shipping Packages</p>
+                        <p>Worldwide Shipping</p>
+                        <p>Interstate Shipping</p>
+                        <p>Local Shipping</p>
+                    </div>
+                    <div className="col-md-3">
+                        <h2>Why Us</h2>
+                        <p>Fast Shipping</p>
+                        <p>Best Customer Service</p>
+                        <p>Very Fast and Reliable</p>
+                        <p>Easy to Track Progress</p>
+                    </div>
+                    <div className="col-md-3">
+                        <h2>Social</h2>
+                        <div className="social-icons">
+                            <FaFacebook className="icon" />
+                            <FaInstagram className="icon" />
+                            <FaLinkedin className="icon" />
+                            <FaGoogle className="icon" />
+                            <FaPinterest className="icon" />
+                            <FaTwitter className="icon" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="footer-bottom bg-secondary py-3">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <span>Our carrier is to fulfill your order and get it at your doorstep</span>
+                        </div>
+                        <div className="col-md-6 text-end">
+                            <div className="logo">
+                                <span className="text-white"><b>sendIT</b></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
+ 
+export default Footer;
+
+
+/* import React from 'react';
+import { FaFacebook, FaInstagram, FaLinkedin, FaGoogle, FaPinterest, FaTwitter } from 'react-icons/fa';
+
+const Footer = () => {
+    return (
         <footer className="footer">
             <div className="footer-content">
                 <div className="footer-section about">
@@ -45,4 +108,4 @@ const Footer = () => {
     );
 }
  
-export default Footer;
+export default Footer; */
